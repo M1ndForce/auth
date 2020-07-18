4a245285d7c0388269f58e02ad224fb880725cc2
legendarynorb@gmail.com <Norby512!>
2020-07-18 22:59:03 +0300

    V 1.0.3

--- app.js
+++ app.js
@@ -1,22 +1,62 @@
 const express = require('express');
-const app = express();
-const bodyParser = require('body-parser')
 const mongoose = require('mongoose');
-
+const passport = require('passport');
+const bodyParser = require('body-parser');
+const User = require('./models/user');
+const LocalStrategy = require('passport-local');
+const passportLocalMongoose = require('passport-local-mongoose');
 mongoose.connect('mongodb+srv://test1:Qwerty512@cluster0.x3p1t.gcp.mongodb.net/auth', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
-    .then(() => console.log('Connected to db /auth !!!!'))
+    .then(() => console.log('Connected to auth!!!!'))
     .catch(error => console.log(error.message));
 
 
+const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
+
+app.use(require('express-session')({
+    secret: 'murchelo is the best cat',
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get('/', (req, res) => {
     res.render('home')
 });
+
+
+app.get('/secret', (req, res) => {
+    res.render('secret')
+});
+
+app.get('/register', (req, res) => {
+    res.render('register')
+});
+
+app.post('/register', (req, res) => {
+    req.body.username
+    req.body.password
+    User.register(new User({username: req.body.username}), req.body.password, (err, user) => {
+        if (err) {
+            console.log(err);
+            return res.render('register');
+        }else {
+            passport.authenticate('local')(req, res, ()=> {
+                res.redirect('/secret');
+            })
+        }
+    });
+});
+
 
 app.listen(3000, () => {
     console.log('Server start')
